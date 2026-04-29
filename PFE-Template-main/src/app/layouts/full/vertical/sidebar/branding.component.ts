5c1fbae2a1afe4527bc21dfe27f6b4736ad1cae5
SirineHk <sirinehkiri8b2@gmail.com>
2026-04-29 05:13:55 +0100

    update sirine

--- PFE-Template-main/src/app/layouts/full/vertical/sidebar/branding.component.ts
+++ PFE-Template-main/src/app/layouts/full/vertical/sidebar/branding.component.ts
@@ -10,7 +10,7 @@ import { CoreService } from 'src/app/services/core.service';
       @if(options.theme === 'light') {
       <a href="/">
         <img
-          src="./assets/images/logos/dark-logo.svg"
+          src="./assets/images/logos/"
           class="align-middle m-2"
           alt="logo"
         />
@@ -18,7 +18,7 @@ import { CoreService } from 'src/app/services/core.service';
       } @if(options.theme === 'dark') {
       <a href="/">
         <img
-          src="./assets/images/logos/light-logo.svg"
+          src="./assets/images/logos/"
           class="align-middle m-2"
           alt="logo"
         />
